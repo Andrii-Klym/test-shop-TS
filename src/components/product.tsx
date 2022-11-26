53881e109195b63e915850b9e7326a5f6e7a9138
dev181 <dev181@d.c>
2022-11-26 08:42:46 +0200

    add products

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,5 +1,16 @@
-export function Product( props: any) {
+import React from 'react';
+import { IProduct } from '../models';
+
+interface ProductProps { 
+    product: IProduct;
+}
+
+export function Product( { product }: ProductProps) {
     return (
-        <div>Product!</div>
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img src={product.image} className="w-1/6" alt={product.title} />
+            <p>{product.title}</p>
+            <p className='font-bold'>{product.price}</p>
+        </div>
     )
 }
