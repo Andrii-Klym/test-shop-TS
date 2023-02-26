3c576deb27801c3c227d9a3b4c937e873fb4a5c0
dev181 <dev181@d.c>
2023-02-26 18:16:58 +0200

    code improvements

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -13,7 +13,6 @@ export function Product( { product }: ProductProps) {
     const btnClasses = ['py-2 px-4 border', btnClassName]
     
     return (
-        // <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
         <div className={`border py-2 px-4 rounded flex flex-col items-center mb-2 ${none}`}>
             <img src={product.image} className="w-1/6" alt={product.title} />
             <p>{product.title}</p>
