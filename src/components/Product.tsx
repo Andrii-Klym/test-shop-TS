c91d8157e77a2eb0767f764f2397a10a81f96ced
dev181 <dev181@d.c>
2023-02-26 18:07:43 +0200

    add delete button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,19 +7,27 @@ interface ProductProps {
 
 export function Product( { product }: ProductProps) {
     const [details, setDetails] = useState(false)
+    const [none, setNone] = useState('')
 
     const btnClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
     const btnClasses = ['py-2 px-4 border', btnClassName]
-
+    
     return (
-        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+        // <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+        <div className={`border py-2 px-4 rounded flex flex-col items-center mb-2 ${none}`}>
             <img src={product.image} className="w-1/6" alt={product.title} />
             <p>{product.title}</p>
             <p className='font-bold'>Price: {product.price}$</p>
-            <button className={btnClasses.join(' ')}
-                    onClick={() => setDetails(prev => !prev)}>
-                {details ? 'Hide Details' : 'Show Details'} 
-            </button>
+            <div className='flex space-x-80'>
+                <button className={btnClasses.join(' ')}
+                        onClick={() => setDetails(prev => !prev)}>
+                    {details ? 'Hide Details' : 'Show Details'} 
+                </button>
+
+                <button className='py-2 px-4 border bg-red-400 hover:text-white'
+                        onClick={() => setNone('hidden')}
+                        >Delete</button>
+            </div>
 
             {details && <div>
                 <p>{product.description}</p>
