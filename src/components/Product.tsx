fb6734d11b768784b01de769f04899d3e57c4abe
dev181 <dev181@d.c>
2023-03-01 11:39:06 +0200

    add editable content

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,15 +8,18 @@ interface ProductProps {
 export function Product( { product }: ProductProps) {
     const [details, setDetails] = useState(false)
     const [none, setNone] = useState('')
-
+    const [title, setTitle] = useState(product.title)
+    const [price, setPrice] = useState(product.price)
+    const [description, setDescription] = useState(product.description)
+    
     const btnClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
     const btnClasses = ['py-2 px-4 border', btnClassName]
     
     return (
         <div className={`border py-2 px-4 rounded flex flex-col items-center mb-2 ${none}`}>
             <img src={product.image} className="w-1/6" alt={product.title} />
-            <p>{product.title}</p>
-            <p className='font-bold'>Price: {product.price}$</p>
+            <p contentEditable onBlur={(event) => setTitle(event.target.innerText)}>{title}</p>
+            <p className='font-bold'>Price: <span contentEditable onBlur={(event) => setPrice(Number(event.target.innerText))}>{price}</span>$</p>
             <div className='flex space-x-80'>
                 <button className={btnClasses.join(' ')}
                         onClick={() => setDetails(prev => !prev)}>
@@ -29,8 +32,8 @@ export function Product( { product }: ProductProps) {
             </div>
 
             {details && <div>
-                <p>{product.description}</p>
-                <p>Rate: <span style={{ fontWeight: "bold"}}>{product?.rating?.rate}</span></p>
+                <p contentEditable onBlur={(event) => setDescription(event.target.innerText)}>{description}</p>
+                <p>Rate: <span contentEditable style={{ fontWeight: "bold"}}>{product?.rating?.rate}</span></p>
             </div>}
         </div>
     )
